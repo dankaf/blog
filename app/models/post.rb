bca531794dcf35a3d96a9c1bb322621fe6fa213a
dankaf <thomastang.thomas97@gmail.com>
2016-08-09 11:57:06 -0700

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Post < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  validates_presence_of :title
+  validates_presence_of :body
+  belongs_to :user
 end
